a6ad4d32007bb134cce620ab10ee2380a7bd1262
Akjol Jaenbai and Julius Bade <akjol@>
2018-10-08 15:17:11 -0700

    add count methods for vowels and consonants and first sentence or 8 words

--- src/journal.js
+++ src/journal.js
@@ -1,5 +1,54 @@
-export function Entry(sentence) {
+export function WordCount(sentence) {
 var words = sentence.split(" ");
 var numberOfWords = words.length;
 return numberOfWords;
+}
+export function VowelCount(sentence) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  var lowerSentence = sentence.toLowerCase();
+  var splitSentence = lowerSentence.split("");
+  var i = 0;
+  splitSentence.forEach(function(letter){
+    vowels.forEach(function(vowel){
+      if(letter === vowel){
+        i += 1;
+      }
+    });
+  });
+  return i;
+}
+  export function ConsonantCount(sentence) {
+    const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+    var lowerSentence = sentence.toLowerCase();
+    var splitSentence = lowerSentence.split("");
+    var x = 0;
+    splitSentence.forEach(function(letter){
+      consonants.forEach(function(consonants){
+        if(letter === consonants){
+          x += 1;
+        }
+      });
+    });
+    return x;
+}
+
+
+
+
+
+
+
+
+export function GetTeaser(sentence) {
+  var stringArray = sentence.split('.');
+  var firstSentenceArray = stringArray[0].split(' ');
+  var returnSentence = "";
+  if (firstSentenceArray.length > 8) {
+    for (var i = 0; i < 8; i++) {
+      returnSentence += i;
+    }
+  } else {
+    returnSentence = firstSentenceArray.join(' ');
+  }
+  return returnSentence;
 }
--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
-import { Entry } from './journal.js';
+import { WordCount } from './journal.js';
+import { VowelCount } from './journal.js';
+import { ConsonantCount } from './journal.js';
+import { GetTeaser } from './journal.js';
 import './styles.css';
 
 $(document).ready(function(){
   $('#journal-form').submit(function(event){
     event.preventDefault();
     var entrySentence = $("#entrySentence").val();
-    var output = Entry(entrySentence);
+    var output = WordCount(entrySentence);
+    var vowelCount = VowelCount(entrySentence);
+    var consonantCount = ConsonantCount(entrySentence);
+    var firstSentence = GetTeaser(entrySentence);
 
-    $("#numberOfWords").append("Number of words : " + output);
+
+    $("#numberOfWords").text("Number of words : " + output);
+    $("#numberOfVowels").text("Number of vowels : " + vowelCount);
+    $("#numberOfConsonants").text("Number of consonants : " + consonantCount);
+    $("#firstSentence").text("First Sentence: " + firstSentence);
 
   });
 });
